0bf0a25a07e95cc66316424b3dde67447c4f4051
Mmishaaa <138068630+Mmishaaa@users.noreply.github.com>
2024-05-30 00:06:16 +0300

    add theme fro about and birthday pages

--- js/BirthdayPage/App/app.js
+++ js/BirthdayPage/App/app.js
@@ -1,9 +1,13 @@
+import ChangeTheme from "./components/ChangeTheme/changeTheme.js";
 import Slider from "./components/Slider/slider.js";
 
 class App {
   init() {
     const slider = new Slider();
     slider.init();
+
+    const changeTheme = new ChangeTheme();
+    changeTheme.init();
   }
 }
 
